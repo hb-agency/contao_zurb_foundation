056c36a31b7cdf930b5d96b2bfb7f2800b7728b6
Blair Winans <blair@winanscreative.com>
2014-11-20 13:06:09 -0500

    Cleanup on Reveal Modal window Element. Now performs more like the core Hyperlink element.

--- library/HBAgency/ContentElement/Foundation/RevealModalWindow.php
+++ library/HBAgency/ContentElement/Foundation/RevealModalWindow.php
@@ -19,6 +19,7 @@
  *
  * Creates a an Modal dialogs, or pop-up windows that can be used at several different sizes.
  * @copyright  2014 HB Agency
+ * @author     Blair Winans <bwinans@hbagency.com>
  * @author     Sebastijan Ribaric <sebastijan.ribaric@media-8.org>
  * @package    Zurb_Foundation
  */
@@ -37,67 +38,55 @@ class RevealModalWindow extends Zurb_Foundation
 	 */
 	protected function compile()
 	{
-		//Compile hyperlink element
-		$this->Template->revHyperlink = $this->_compileHyperlink();
-
-		
-		// Compile article element
-		$articles = empty($this->foundation_articles)
-		? null
-		: deserialize($this->foundation_articles);
-		
-		$this->Template->revIncludeArticle = '';
-		if (!empty($articles))
-		foreach ($articles as $item)
+		//Compile hyperlink element fields
+		$this->_compileHyperlink();
+        
+        //Compile articles
+		if($this->foundation_incarticle)
 		{
-			if (isset($item['article']))
-				$this->Template->revIncludeArticle .= $this->getArticle($item['article']). "\n";
+    		$strArticles = '';
+    		foreach(deserialize($this->foundation_article, true) as $arrArticle)
+    		{
+        		if (!empty($arrArticle['article']))
+        		{
+            		$strArticles .= $this->getArticle($arrArticle['article'], false, true). "\n";
+        		}
+    		}
+    		
+    		$this->Template->articles = $strArticles;
 		}
-				
 		
-		// Compile module element
-		$module =  empty($this->module)
-			? null
-			: $this->module;
-		$this->Template->revIncludeModule = $this->getFrontendModule($module);
-
-		
-		// Link trigger style
-		$this->Template->revealModalButt = $this->foundation_button;
-		$this->Template->headline = '';
-		$this->headline = '';
-		$this->hl = '';
-
+		//Compile modules
+		if($this->foundation_incmodule)
+		{
+    		$strModules = '';
+    		foreach(deserialize($this->foundation_modules, true) as $arrModule)
+    		{
+        		if (!empty($arrModule['module']))
+        		{
+            		$strModules .= $this->getFrontendModule($arrModule['module']). "\n";
+        		}
+    		}
+    		
+    		$this->Template->modules = $strModules;
+		}
 	}
 	
 	/**
-	 * Generate the hyperlink module
+	 * Generate the content element
+	 * This is mostly adapted from Contao\ContentHyperlink::compile()
 	 */
 	protected function _compileHyperlink()
 	{
 		global $objPage;
-		
-		if (substr($this->url, 0, 7) == 'mailto:')
-		{
-			$this->url = \String::encodeEmail($this->url);
-		}
-		else
-		{
-			$this->url = ampersand($this->url);
-		}
-		
+
 		$embed = explode('%s', $this->embed);
-		
-		if ($this->linkTitle == '')
-		{
-			$this->linkTitle = $this->url;
-		}
-		
+
 		// Use an image instead of the title
 		if ($this->useImage && $this->singleSRC != '')
 		{
 			$objModel = \FilesModel::findByUuid($this->singleSRC);
-		
+
 			if ($objModel === null)
 			{
 				if (!\Validator::isUuid($this->singleSRC))
@@ -107,65 +96,47 @@ class RevealModalWindow extends Zurb_Foundation
 			}
 			elseif (is_file(TL_ROOT . '/' . $objModel->path))
 			{
-				$this->subTemplate = new \FrontendTemplate('ce_hyperlink_image');
-				$this->subTemplate->setData($this->arrData);
-		
 				$objFile = new \File($objModel->path, true);
-		
+
 				if ($objFile->isGdImage)
 				{
 					$size = deserialize($this->size);
 					$intMaxWidth = (TL_MODE == 'BE') ? 320 : \Config::get('maxImageWidth');
-		
+
 					// Adjust the image size
 					if ($intMaxWidth > 0  && ($size[0] > $intMaxWidth || (!$size[0] && $objFile->width > $intMaxWidth)))
 					{
 						$size[0] = $intMaxWidth;
 						$size[1] = floor($intMaxWidth * $objFile->height / $objFile->width);
 					}
-		
+
 					$src = \Image::get($objModel->path, $size[0], $size[1], $size[2]);
-		
+
 					if (($imgSize = @getimagesize(TL_ROOT . '/' . rawurldecode($src))) !== false)
 					{
-						$this->subTemplate->arrSize = $imgSize;
-						$this->subTemplate->imgSize = ' ' . $imgSize[3];
+						$this->Template->arrSize = $imgSize;
+						$this->Template->imgSize = ' ' . $imgSize[3];
 					}
-		
-					$this->subTemplate->src = TL_FILES_URL . $src;
-					$this->subTemplate->alt = specialchars($this->alt);
-					$this->subTemplate->linkTitle = specialchars($this->linkTitle);
-					$this->subTemplate->caption = $this->caption;
+
+					$this->Template->src = TL_FILES_URL . $src;
+					$this->Template->alt = specialchars($this->alt);
+					$this->Template->linkTitle = specialchars($this->linkTitle);
+					$this->Template->caption = $this->caption;
 				}
 			}
-		} else {
-			
-			$this->subTemplate = new \FrontendTemplate('ce_hyperlink');
-			$this->subTemplate->setData($this->arrData);
 		}
-		
-		$this->subTemplate->rel = $this->rel; // Backwards compatibility
-		$this->subTemplate->href = $this->url;
-		$this->subTemplate->embed_pre = $embed[0];
-		$this->subTemplate->embed_post = $embed[1];
-		$this->subTemplate->link = $this->linkTitle;
-		$this->subTemplate->linkTitle = specialchars($this->titleText ?: $this->linkTitle);
-		$this->subTemplate->target = '';
-		
-		
-		// Override the link target
-		if ($this->target)
-		{
-			$this->subTemplate->target = ($objPage->outputFormat == 'xhtml') ? ' onclick="return !window.open(this.href)"' : ' target="_blank"';
-		}
-		
+
+		$this->Template->embed_pre = $embed[0];
+		$this->Template->embed_post = $embed[1];
+		$this->Template->link = $this->linkTitle;
+		$this->Template->linkTitle = specialchars($this->titleText ?: $this->linkTitle);
+
 		// Unset the title attributes in the back end (see #6258)
 		if (TL_MODE == 'BE')
 		{
-			$this->subTemplate->title = '';
-			$this->subTemplate->linkTitle = '';
+			$this->Template->title = '';
+			$this->Template->linkTitle = '';
 		}
-		
-		return $this->subTemplate->parse();
 	}
+
 }
