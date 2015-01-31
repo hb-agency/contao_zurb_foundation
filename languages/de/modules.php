713d00a668c52139e18c8cde6bb515dbc99945bd
Alexander Bröss <broess@zweikraftwerk.de>
2015-01-31 07:49:49 +0100

    Adding missing TL_Lang entries
    TL_Lang entries for foundation_iconbar and foundation_revealmodalwindow

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -14,3 +14,5 @@
 $GLOBALS['TL_LANG']['FMD']['foundationnav_topbar'] = array('Foundation Top Bar', 'Erstellt ein "Top Bar" Navigationsmodule mit dem Zurb Foundation Framework.');
 $GLOBALS['TL_LANG']['FMD']['foundation_offcanvas'] = array('Foundation Off Canvas', 'Erstellt ein "Off-Canvas" Module mit dem Zurb Foundation Framework.');
 $GLOBALS['TL_LANG']['FMD']['foundation_tabbar']    = array('Foundation Tab Bar', 'Erstellt eine Tab-Leiste mit dem Zurb Foundation Framework, das für ein Off-Canvas Menu verwandt werden kann.');
+$GLOBALS['TL_LANG']['FMD']['foundation_revealmodalwindow']   = array('Foundation Reveal Modal', 'Erstellt einen Modal Dialog oder ein Pop-Up Fenster, dass in verschiedenen Größen genutzt werden kann.');
+$GLOBALS['TL_LANG']['FMD']['foundation_iconbar']   = array('Foundation Icon Bar', 'Erstellt eine Foundation Icon Leiste.');
--- languages/en/modules.php
+++ languages/en/modules.php
@@ -14,3 +14,5 @@
 $GLOBALS['TL_LANG']['FMD']['foundationnav_topbar'] = array('Foundation Top Bar', 'Creates a "top bar" navigation module using the Zurb Foundation framework.');
 $GLOBALS['TL_LANG']['FMD']['foundation_offcanvas'] = array('Foundation Off Canvas', 'Creates a "off-canvas" module using the Zurb Foundation framework.');
 $GLOBALS['TL_LANG']['FMD']['foundation_tabbar']    = array('Foundation Tab Bar', 'Creates a tab bar using the Zurb Foundation framework, and can be used to trigger an off-canvas menu.');
+$GLOBALS['TL_LANG']['FMD']['foundation_revealmodalwindow']   = array('Foundation Reveal Modal', 'Creates a an Modal dialogs, or pop-up windows that can be used at several different sizes.');
+$GLOBALS['TL_LANG']['FMD']['foundation_iconbar']   = array('Foundation Icon Bar', 'Creates a Foundation Icon Bar.');
